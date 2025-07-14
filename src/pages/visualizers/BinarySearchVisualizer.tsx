2185564e80d54ad44eb9a45e3b07ce46215e1c4d
manas-jain09 <112091116+manas-jain09@users.noreply.github.com>
2025-07-14 21:14:10 +0530

    Update BinarySearchVisualizer.tsx

--- src/pages/visualizers/BinarySearchVisualizer.tsx
+++ src/pages/visualizers/BinarySearchVisualizer.tsx
@@ -220,7 +220,7 @@ const BinarySearchVisualizer = () => {
       
       <div className="page-container mt-20">
         <div className="mb-8">
-          <Link to="/algorithms" className="flex items-center text-drona-green hover:underline mb-4 font-medium">
+          <Link to="/dashboard/algorithms" className="flex items-center text-drona-green hover:underline mb-4 font-medium">
             <ArrowLeft className="mr-2 h-4 w-4" />
             Back to Algorithms
           </Link>
